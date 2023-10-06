338b3b5ccec28129d50e5269fbcabfca1e42fb27
davidbern <142316776+Burnhulk@users.noreply.github.com>
2023-10-06 17:35:08 +0200

    Update betting_game.py

--- betting_game.py
+++ betting_game.py
@@ -1,5 +1,8 @@
 import requests
-#get NFL Boxscores through API
+import tkinter as tk
+
+week = str(input("Which week?"))
+#Get Box Scores through API
 def get_weekly_scores(week):
     url = "https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLScoresOnly"
 
@@ -18,41 +21,74 @@ def get_weekly_scores(week):
         print(f"Error: Unable to retrieve data for week {week}. Status code {response.status_code}")
         return None
 
+#show all the games and the scores
+def show_games(response):
+    
+    games = response["body"]
 
-#find out Winners/losers of the Week
-def results(response):
+    for game_id, details in games.items():
+        away_team = details["away"]
+        home_team = details["home"] 
+        away_score = details["awayPts"]
+        home_score = details["homePts"] 
+
+        try: 
+            print(f"{away_team} {away_score} - {home_score} {home_team}")
+        except:
+            print(f"{away_team} - {home_team}")
+
+#find the winners from games
+def winner(response):
+    
     winners_list = []
-    losers_list = []
     draw_list = []
 
     games = response["body"]
 
-    for game_id, details in games.items():
-        away_team = details['away']
-        home_team = details['home'] 
-        away_score = details['awayPts']
-        home_score = details['homePts']   
+    for game_id, details in response["body"].items():
 
-        if int(home_score) > int(away_score):
-            winners_list.append(home_team)
-            losers_list.append(away_team)
-
-        elif int(home_score) == int(away_score):
-            draw_list.append([home_team, away_team])
-
-        else:
-            winners_list.append(away_team)
-            losers_list.append(home_team) 
-
-    return winners_list, losers_list
-
-response_data = get_weekly_scores(1)
-if response_data:
-    winners_list, losers_list = results(response_data)
-
-    print("Winner:", winners_list)
-    print("Loser:", losers_list)
+        away_team = details["away"]
+        home_team = details["home"] 
+        away_score = details["awayPts"]
+        home_score = details["homePts"] 
+        
+        try:#logic to find the winners. int is necessary otherwise it wont work
+            if int(home_score) > int(away_score):
+                winners_list.append(home_team)
+            
+            elif int(home_score) == int(away_score):
+                draw_list.append(home_team, away_team)
+            
+            else:
+                winners_list.append(away_team)
+        except ValueError:
+            print("No Games played yet")
+        
+    return winners_list, draw_list
 
 
+#WiP to show results
+response = get_weekly_scores(week)
+show_games(response)
+winners_list, draw_list = winner(response)
+print(winners_list)
 
+
+    
+    
+
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.win_tips = {}
+        self.points = 0
+    def add_win_tip(self, details, tip):
+        pass
+    def player_points(self):
+        pass
+
+
+    
 
